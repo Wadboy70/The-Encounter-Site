bfb4ac523468c50face2a64d2e5f4a0a360c4810
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-10-30 14:04:18 -0400

    heroku changes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 3001;
-console.log(process.env)
 
 const publicPath = path.join(__dirname, '..','build');
 
@@ -11,7 +10,6 @@ app.use(express.static(publicPath));
 
 app.get('*', (req,res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
-    console.log(publicPath);
 });
 
 app.listen(port, () => {
