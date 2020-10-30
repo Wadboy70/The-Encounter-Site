2dd9d2b18eedba28d6ebc5693c19d51276201d77
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-10-30 19:09:39 -0400

    heroku changes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv').config();
-const port = process.env.PORT || 3001;
-
+require('dotenv').config({path:__dirname+'/./../.env'});
 const publicPath = path.join(__dirname, '..','build');
 
 app.use(express.static(publicPath));
@@ -12,6 +10,7 @@ app.get('*', (req,res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Server is running on port ${port} :)`);
 });
