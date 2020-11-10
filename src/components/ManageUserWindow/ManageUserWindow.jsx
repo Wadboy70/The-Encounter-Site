24d8885435f9f039ee6f3485a44bcaecf8635709
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-11-09 20:34:24 -0500

    admin panel is done

--- src/components/ManageUserWindow/ManageUserWindow.jsx
+++ src/components/ManageUserWindow/ManageUserWindow.jsx
@@ -7,33 +7,35 @@ import './ManageUserWindow.scss';
 const ManageUserWindow = ({selectedUser = {}, changeUserTier}) => {
     const {email, displayName, date, tier} = selectedUser;
     return (
-        <table className = 'manageUserWindow'>
-            {
-                email &&
-                <thead>
-                    
-                    <tr>
-                        <th>{email}</th>
-                        <th>{displayName}</th>
-                        <th>{parseFirestoreDate(date)}</th>
-                        <th>
-                            <select aria-label = 'tiers' name="tiers" id="tierList" value = {tier} onChange = {changeUserTier}>
-                                {
-                                    Object.keys(USER_TIERS).map(tierChoice => (
-                                        <option 
-                                            key = {tierChoice} 
-                                            value = {tierChoice}
-                                        >
-                                            {titleCaseSentence(tierChoice)}
-                                        </option>
-                                    ))
-                                }
-                            </select>
-                        </th>
-                    </tr>
-                </thead>
-            }
-        </table>
+        <div className = 'manageUserWindow'>
+            <table>
+                {
+                    email &&
+                    <thead>
+                        
+                        <tr>
+                            <th>{email}</th>
+                            <th>{displayName}</th>
+                            <th>{parseFirestoreDate(date)}</th>
+                            <th>
+                                <select aria-label = 'tiers' name="tiers" id="tierList" value = {tier} onChange = {changeUserTier}>
+                                    {
+                                        Object.keys(USER_TIERS).map(tierChoice => (
+                                            <option 
+                                                key = {tierChoice} 
+                                                value = {tierChoice}
+                                            >
+                                                {titleCaseSentence(tierChoice)}
+                                            </option>
+                                        ))
+                                    }
+                                </select>
+                            </th>
+                        </tr>
+                    </thead>
+                }
+            </table>
+        </div>
     )
 };
 
