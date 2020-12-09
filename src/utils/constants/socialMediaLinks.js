83c37eaeb723f5494ac58e5696543c0a06ce839e
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-12-08 23:14:55 -0500

    made a lot of cosmetic changes and made links clickable

---
+++ src/utils/constants/socialMediaLinks.js
@@ -0,0 +1,5 @@
+export default {
+    instagram: 'instagram.com',
+    facebook: 'https://www.facebook.com/TheEncounterWorshipCenter',
+    youtube: 'https://www.youtube.com/user/arthurhintonmusic'
+};
