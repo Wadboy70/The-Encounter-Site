3436e054f33a3bfd22d8e23e4c2a01ed59800fb2
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-12-13 13:47:16 -0500

    logo in slideshow is updated, image and text in pastor arthurs bio are updated

--- src/containers/AboutPastorSection/AboutPastorSection.jsx
+++ src/containers/AboutPastorSection/AboutPastorSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import DisplayItem from '../../components/DisplayItem/DisplayItem';
 import HighlightSection from '../HighlightSection/HighlightSection';
-import arthur from '../../assets/images/arthurHeadshot.jpg';
+import arthur from '../../assets/images/arthurCollage.png';
 
 import './AboutPastorSection.scss';
 
@@ -20,7 +20,7 @@ const AboutPastorSection = () => {
                 className = 'aboutPastorSection__displayItem'
             >
                 <p>
-                    Arthur T. Hinton III is a Pastor, Songwriter, Musician, and Behavioral Psychologist.  As the founder and lead Pastor at The Encounter Worship Center it is his life's purpose to create limitless opportunities for all people from all walks of life to encounter God, so that their lives will be forever changed.  He holds a Bachelors degree from Millikin University in Organizational Leadership and a Master's Degree from Kaplan University in Psychology with a concentration in Applied Behavior Analysis.  Pastor Arthur resides in Tampa, Florida and has Four beautiful children, Justus, Adrienne, Liberty, and Arthur IV.
+                    Arthur T. Hinton III is a Pastor, Songwriter, Musician, and Behavioral Psychologist.  As the founder and lead Pastor at The Encounter Worship Center, it is his life's purpose to create limitless opportunities for all people from all walks of life to encounter God, so that their lives will be forever changed.  He holds a Bachelor of Science from Millikin University in Organizational Leadership and a Master of Science from Kaplan University in Psychology with a concentration in Applied Behavior Analysis.  Pastor Arthur resides in Tampa, Florida and has four beautiful children, Justus, Adrienne, Liberty, and Arthur IV.
                 </p>
             </DisplayItem>
         </HighlightSection>        
