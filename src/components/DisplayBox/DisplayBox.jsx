cec5e351d910eaca2aef4a0a4af7b927b730b790
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-12-14 22:04:49 -0500

    display section has been fixed

---
+++ src/components/DisplayBox/DisplayBox.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from '../Button/Button';
+
+import './DisplayBox.scss';
+
+const DisplayBox = ({
+    className = '',
+    title,
+    button, 
+    image,
+    description, 
+    buttonProps = {}
+}) => {
+    console.log(image);
+    return(
+        <div className = {`displayBox ${className}`} style = {image ? {backgroundImage: `url(${image})`} : undefined}>
+            <div className = 'displayBox__bg'>
+                {
+                    title &&
+                    <h2 className = 'displayBox__title'>{title}</h2>
+                }
+                {
+                    description &&
+                    <p className = 'displayBox__description'>{description}</p>
+                }
+                {
+                    button  &&
+                    <Button {...buttonProps} className = 'displayBox__button small yellowBG'>{button}</Button>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default DisplayBox;
--- src/containers/DisplaySection/DisplaySection.jsx
+++ src/containers/DisplaySection/DisplaySection.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-
-import DisplayItem from '../../components/DisplayItem/DisplayItem';
-import Kid from '../../assets/images/Kid.svg';
-import Coin from '../../assets/images/coins.svg';
-import Computer from '../../assets/images/computer.svg';
+import DisplayBox from '../../components/DisplayBox/DisplayBox';
+import youthImage from '../../assets/images/youthImage.jpg';
+import churchService from '../../assets/images/churchService.jpg';
+import tithing from '../../assets/images/tithing.jpg';
 import ROUTES from '../../utils/routes';
 
 import './DisplaySection.scss';
@@ -11,36 +10,32 @@ import './DisplaySection.scss';
 const DisplaySection = () => {
     const displayContent = [
         {
-            icon: Kid,
             title: 'Engage Youth Ministry',
-            content: 'The Encounter Worship Center\'s Youth Ministry "Engage" has been designed to help all youth from elementary, middle, and high school age groups to engage God the father not just on Sundays, but in their everyday lives. Through our teaching outlets it is our expectation that our youth will grow in their faith, their character, and their relationships.',
             button: 'Find Out More',
-            buttonProps: {}
+            description: 'Engaging programming to help our youth grow in their faith',
+            image: youthImage,
+            buttonProps: {link: ROUTES.YOUTH_MINISTRY.url}
         },
         {
-            icon: Coin,
             title: 'Giving/ Tithing',
-            content: 'Your generosity is positively changing lives in this ministry, community, and globally.  The Apostle Paul reminds us in 2 Corinthians 9:6-8 that whoever sows sparingly will reap sparingly and whosoever sows generously will reap generously.  As you generously sow into this work, it is Gods covenant promise to multiply back to you what has been given.  We want you to know that your seed creates resources for us to carry Gods love to the nations',
             button: 'Give',
-            buttonProps: {anchor: 'https://www.givelify.com/givenow/1.0/NzExOTk=/selection'}
+            buttonProps: {anchor: 'https://www.givelify.com/givenow/1.0/NzExOTk=/selection'},
+            description: 'Your generosity is positively changing lives in this ministry, community, and globally',
+            image: tithing
         },
         {
-            icon: Computer,
             title: 'Live Streams',
-            content: 'Join our weekly livestreams where we have receive worship and wise sermons from our Pastor, Arthur T. Hinton.',
             button: 'Watch Now',
-            buttonProps: {link: ROUTES.LIVE_SERMONS.url}
+            description: 'Join our weekly livestreams where we have receive worship and wise sermons from our Pastor, Arthur T. Hinton',
+            buttonProps: {link: ROUTES.LIVE_SERMONS.url},
+            image: churchService
         }
     ];
     return(
         <section className = 'display'>
             {
                 displayContent.map((display, index) => (
-                    <DisplayItem key = {index} info = {display} className = 'display__item'>
-                        <p>
-                            {display.content}
-                        </p>
-                    </DisplayItem>
+                    <DisplayBox key = {index} {...display} className = 'display__box'/>
                 ))
             }
         </section>
