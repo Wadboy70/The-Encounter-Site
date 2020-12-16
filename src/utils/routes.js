adbb41d31085d819eb7ccec4f725103ea0cb35f6
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-12-15 22:38:48 -0500

    all of the form page content is written, now to render it all

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -11,6 +11,119 @@ const ROUTES = {
     MUSIC_MINISTRY: {name: 'New Wine Music Ministry', url: '/musicministry'},
     WOMEN_MINISTRY: {name: 'Leader of Esthers Circle Woman’s Group', url: '/womensministry'},
     YOUTH_MINISTRY: {name: 'Engage Youth Ministry', url: '/youthministry'},
+    GIVING: {name: 'Giving', url: '/giving'}
 };
+export const FORM_SUBMIT_TYPE = {
+    EMAIL: 'EMAIL',
+    ADMIN_STORAGE: 'ADMIN_STORAGE'
+};
+
+export const FORM_FIELD_INPUT_TYPE = {
+    TEXT_INPUT: 'TEXT_INPUT',
+    TEXT_AREA: 'TEXT_AREA' 
+};
+
+export const LINK_BUTTON_TYPES = {
+    INTERNAL: 'INTERNAL',
+    EXTERNAL: 'EXTERNAL' 
+};
+export const DUPLICATE_PAGES = [
+    {
+        jumboImage: undefined,
+        title: ROUTES.MEN_MINISTRY.name,
+        url: ROUTES.MEN_MINISTRY.url,
+        description: 'Minister Anthony Alexander is the leader of our Guided Men’s Ministry! Sign up for this program using the form below',
+        form:{
+            submit: FORM_SUBMIT_TYPE.ADMIN_STORAGE,
+            fields: [
+                {
+                    name: 'name',
+                    label: 'Name',
+                    type: FORM_FIELD_INPUT_TYPE.TEXT_INPUT
+                },
+                {
+                    name: 'email',
+                    label: 'Email',
+                    type: FORM_FIELD_INPUT_TYPE.TEXT_INPUT
+                }
+            ],
+        }
+
+    },
+    {
+        jumboImage: undefined,
+        title: ROUTES.GIVING.name,
+        url: ROUTES.GIVING.url,
+        description: 'Your generosity is positively changing lives in this ministry, community, and globally. The Apostle Paul reminds us in 2 Corinthians 9:6-8 that whoever sows sparingly will reap sparingly and whosoever sows generously will reap generously. As you generously sow into this work, it is Gods covenant promise to multiply back to you what has been given. We want you to know that your seed creates resources for us to carry Gods love to the nations',
+        link: {
+            type: LINK_BUTTON_TYPES.EXTERNAL,
+            to: 'https://www.givelify.com/givenow/1.0/NzExOTk=/selection'
+        }
+    },
+    {
+        jumboImage: undefined,
+        title: ROUTES.WOMEN_MINISTRY.name,
+        url: ROUTES.WOMEN_MINISTRY.url,
+        description: 'Minister Lavon Thompson is the leader of our Esthers Circle Woman’s Group! Sign up for this program using the form below',
+        form:{
+            submit: FORM_SUBMIT_TYPE.ADMIN_STORAGE,
+            fields: [
+                {
+                    name: 'name',
+                    label: 'Name',
+                    type: FORM_FIELD_INPUT_TYPE.TEXT_INPUT
+                },
+                {
+                    name: 'email',
+                    label: 'Email',
+                    type: FORM_FIELD_INPUT_TYPE.TEXT_INPUT
+                }
+            ],
+        }
+    },
+    {
+        jumboImage: undefined,
+        title: ROUTES.YOUTH_MINISTRY.name,
+        url: ROUTES.YOUTH_MINISTRY.url,
+        description: 'Minister Julianna James-Gilbert is the leader of our Engage Youth Ministry! Sign up for this program using the form below',
+        form:{
+            submit: FORM_SUBMIT_TYPE.ADMIN_STORAGE,
+            fields: [
+                {
+                    name: 'name',
+                    label: 'Name',
+                    type: FORM_FIELD_INPUT_TYPE.TEXT_INPUT
+                },
+                {
+                    name: 'email',
+                    label: 'Email',
+                    type: FORM_FIELD_INPUT_TYPE.TEXT_INPUT
+                }
+            ],
+        }
+    },
+    {
+        jumboImage: undefined,
+        title: ROUTES.MUSIC_MINISTRY.name,
+        url: ROUTES.MUSIC_MINISTRY.url,
+        description: 'Minister Brittany C. Weaver is the leader of our New Wine Music Ministry! Sign up for this program using the form below',
+        form:{
+            submit: FORM_SUBMIT_TYPE.ADMIN_STORAGE,
+            fields: [
+                {
+                    name: 'name',
+                    label: 'Name',
+                    type: FORM_FIELD_INPUT_TYPE.TEXT_INPUT
+                },
+                {
+                    name: 'email',
+                    label: 'Email',
+                    type: FORM_FIELD_INPUT_TYPE.TEXT_INPUT
+                }
+            ],
+        }
+    },
+];
 export default ROUTES;
 export const siteMap = [ROUTES.ABOUT, ROUTES.LIVE_SERMONS, ROUTES.CALENDAR, ROUTES.PRAYER_REQUESTS, ROUTES.CONTACT_US];
+
