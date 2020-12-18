c0a9a0bde8c8e378cd20c95a18ab3762cdec098a
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-12-18 01:37:48 -0500

    the only thing left is to add deleting users from listings

--- src/containers/ManageEventSignUp/ManageEventSignUp.jsx
+++ src/containers/ManageEventSignUp/ManageEventSignUp.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 
 import { getAllDocs } from '../../utils/firebase';
 import { DUPLICATE_PAGES, FORM_SUBMIT_TYPE } from '../../utils/routes';
+import Button from '../../components/Button/Button';
 
 import './ManageEventSignUp.scss';
+import { copyToClipboard } from '../../utils/helperFunctions';
 
 const ManageEventSignUp = () => {
     const [programs, setPrograms] = useState(undefined);
@@ -29,19 +31,39 @@ const ManageEventSignUp = () => {
 
         return () => mounted = false;
     })
+    const copyEmails = program => {
+        let emails = program.map(val => val.email).join(',');
+        if(emails !== '') {
+            copyToClipboard(emails);
+            alert('Emails were copied!');
+        }
+    }
     return(
         <div className = 'manageEventSignUp'>
+            <h1 className = 'manageEventSignUp__title'>Manage Program Sign Ups</h1>
             {
                 programs &&
                 Object.keys(programs).map(program => (
                     <div key = {program} className = 'manageEventSignUp__program'>
-                        <h2 className = 'program__title'>{program}</h2>
+                        <span className = 'program__headerInfo'>
+                            <h2 className = 'program__title'>{program}</h2>
+                            <Button 
+                                op = {() => copyEmails(programs[program])}
+                                className = 'transparent navyBorder small program__button'
+                            >Copy emails</Button>
+                        </span>
+                        
                         <ul className = 'program__entries'>
                             {
                                 programs[program].map(member => (
                                     <li key = {member.name} className = 'entry__member'>
                                         <span className = 'member__name'>{member.name}</span>
                                         <span className = 'member__email'>{member.email}</span>
+                                        <span>
+                                            <Button 
+                                                className = 'transparent small member__button'
+                                            >X</Button>
+                                        </span>
                                     </li>
                                 ))
                             }
--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -58,3 +58,27 @@ export const toMilitaryTime = (hour, noonTime) => {
   let realHour = (Number(hour) === 12) ? 0 : hour;
   return noonTime === 'am' ? realHour : Number(realHour) + 12;
 };
+
+
+//From hackerNoon
+//https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
+
+export const copyToClipboard = str => {
+  const el = document.createElement('textarea');  
+  el.value = str;                                 
+  el.setAttribute('readonly', '');                
+  el.style.position = 'absolute';                
+  el.style.left = '-9999px';                      
+  document.body.appendChild(el);                  
+  const selected =           
+    document.getSelection().rangeCount > 0        
+      ? document.getSelection().getRangeAt(0)     
+      : false;                                    
+  el.select();                                    
+  document.execCommand('copy');                   
+  document.body.removeChild(el);                  
+  if (selected) {                                 
+    document.getSelection().removeAllRanges();    
+    document.getSelection().addRange(selected);   
+  }
+};   
