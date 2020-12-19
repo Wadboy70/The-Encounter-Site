40b501dc52b214c16b821c62d874594150dbb4a8
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-12-18 19:31:34 -0500

    pastor arthur changes

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -19,7 +19,7 @@ const ROUTES = {
     YOUTH_MINISTRY: {name: 'Engage Youth Ministry', url: '/youthministry'},
     MEN_MINISTRY: {name: 'Guided Men\'s Ministry', url: '/mensministry'},
     MUSIC_MINISTRY: {name: 'New Wine Music Ministry', url: '/musicministry'},
-    WOMEN_MINISTRY: {name: 'Leader of Esthers Circle Woman’s Group', url: '/womensministry'},
+    WOMEN_MINISTRY: {name: 'Leader of Esther’s Circle Woman’s Group', url: '/womensministry'},
 };
 export const FORM_SUBMIT_TYPE = {
     EMAIL: 'EMAIL',
@@ -40,7 +40,7 @@ export const DUPLICATE_PAGES = [
         jumboImage: imageUrls.churchService,
         title: ROUTES.MEN_MINISTRY.name,
         url: ROUTES.MEN_MINISTRY.url,
-        description: 'Sign up for this program using the form below',
+        description: 'Sign up for this ministry using the form below',
         form:{
             submit: {
                 type: FORM_SUBMIT_TYPE.ADMIN_STORAGE,
@@ -63,7 +63,7 @@ export const DUPLICATE_PAGES = [
     },
     {
         jumboImage: imageUrls.tithing,
-        title: ROUTES.GIVING.name,
+        title: 'ways to give',
         url: ROUTES.GIVING.url,
         description: 'Your generosity is positively changing lives in this ministry, community, and globally. The Apostle Paul reminds us in 2 Corinthians 9:6-8 that whoever sows sparingly will reap sparingly and whosoever sows generously will reap generously. As you generously sow into this work, it is Gods covenant promise to multiply back to you what has been given. We want you to know that your seed creates resources for us to carry Gods love to the nations',
         link: {
@@ -76,7 +76,7 @@ export const DUPLICATE_PAGES = [
         jumboImage: imageUrls.womensMinistry,
         title: ROUTES.WOMEN_MINISTRY.name,
         url: ROUTES.WOMEN_MINISTRY.url,
-        description: 'Sign up for this program using the form below',
+        description: 'Sign up for this ministry using the form below',
         form:{
             submit: {
                 type: FORM_SUBMIT_TYPE.ADMIN_STORAGE,
@@ -101,7 +101,7 @@ export const DUPLICATE_PAGES = [
         title: ROUTES.YOUTH_MINISTRY.name,
         url: ROUTES.YOUTH_MINISTRY.url,
         description: <>{'The Encounter Worship Center\'s Youth Ministry "Engage" has been designed to help all youth from elementary, middle, and high school age groups to engage God the father not just on Sundays but in their everyday lives and young believers.  Through our teaching outlets, and fellowship opportunities it is our expectation that our youth will grow in their faith, their character, and their relationships.'} <br/><br/>{
-            <span>Sign up for this program using the form below</span>}</>,
+            <span>Sign up for this ministry using the form below</span>}</>,
         form:{
             submit: {
                 type: FORM_SUBMIT_TYPE.ADMIN_STORAGE,
@@ -125,7 +125,7 @@ export const DUPLICATE_PAGES = [
         jumboImage: imageUrls.concert,
         title: ROUTES.MUSIC_MINISTRY.name,
         url: ROUTES.MUSIC_MINISTRY.url,
-        description: 'Sign up for this program using the form below',
+        description: 'Sign up for this ministry using the form below',
         form:{
             submit: {
                 type: FORM_SUBMIT_TYPE.ADMIN_STORAGE,
@@ -178,7 +178,7 @@ export const DUPLICATE_PAGES = [
         jumboImage: imageUrls.friends,
         title: ROUTES.PRAYER_REQUESTS.name,
         url: ROUTES.PRAYER_REQUESTS.url,
-        description: 'Submit prayer requests for the leaders of our ministry using the form below',
+        description: 'Submit prayer requests to the leaders of our ministry using the form below',
         form:{
             submit: {
                 type: FORM_SUBMIT_TYPE.EMAIL,
