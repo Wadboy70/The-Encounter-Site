808ee8b19db3a371f3464db912345c9acdb1d8ab
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-12-20 17:53:28 -0500

    file upload is coming along

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Button from '../../components/Button/Button';
 
 import ManageEventSignUp from '../../containers/ManageEventSignUp/ManageEventSignUp';
 import ManageUsers from '../../containers/ManageUsers/ManageUsers';
 import withSpinner from '../../utils/hocs/withSpinner';
 import withUserRedirect from '../../utils/hocs/withUserRedirect';
-import {fileUpload} from '../../utils/firebase';
+import ManageTitheFile from '../../containers/ManageTitheFile/ManageTitheFile';
 
 import './AdminPage.scss';
 
@@ -18,12 +17,5 @@ const AdminPage = () => {
         </div>
     )
 };
-
-const ManageTitheFile = () => (
-    <form >
-        <input type="file" onChange = {e => fileUpload(e.target.files[0], e.target.files[0].name)}/>
-        <Button>Submit</Button>
-    </form>
-)
 
 export default withUserRedirect(withSpinner(AdminPage));
